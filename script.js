648b383c58c99a085f41f8d80215967585bd5685
John Nilsson <John@mini.(none)>
2009-09-16 23:43:50 +0200

    Adding an evaluation rule to add "classes"

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // http://dev.opera.com/articles/view/rich-html-editing-in-the-browser-part-2/
-
+// http://dev.w3.org/html5/spec/Overview.html#editing-apis
 //DONE: 
 // evaluation The Humane Environment-style
 
@@ -10,8 +10,6 @@
 // GUID for identifier declarations 
 // href and class? for references to enable refactoring inline expression
 
-
-	
 $(document).ready(function() {
 	c = $("#content");
 	c.bind('keydown', 'ctrl+space', auto_eval);
@@ -19,20 +17,35 @@ $(document).ready(function() {
 
 function auto_eval(e) {
 	var rng = window.getSelection().getRangeAt(0);
-	var val = eval(rng.toString());
-	if(!!val)
-		replace(rng,window.document.createTextNode(val));
+	var cmd = commands[rng.toString()];
+	if (!!cmd) {
+		rng.deleteContents();
+		alert(cmd());
+		rng.insertNode(cmd());
+	} else {
+		var val = eval(rng.toString());
+		if (!!val)
+			replace(rng, window.document.createTextNode(val));
+	}
 	return false;
 }
 
+var commands = {
+	"class" : create_class_node
+}
 
+function create_class_node() {
+	var div = document.createElement('div');
+	div.innerHTML = '<h2>classname</h2><dl><dt>methodName</dt><dd>methodBody</dd></dl>';
+	return div;
+}
 
 //
 // Random utils picked up around the net...
 //
 
 function isTextNode(node) {
-	return node.nodeType==3;
+	return node.nodeType == 3;
 }
 function rightPart(node, ix) {
 	return node.splitText(ix);
@@ -46,23 +59,24 @@ function leftPart(node, ix) {
 function replace(rng, node) {
 	rng.deleteContents();
 	if (isTextNode(rng.startContainer)) {
-		var refNode = rightPart(rng.startContainer, rng.startOffset)		
+		var refNode = rightPart(rng.startContainer, rng.startOffset);
 		refNode.parentNode.insertBefore(node, refNode);
 	} else {
-		if (rng.startOffset==rng.startContainer.childNodes.length) {
+		if (rng.startOffset == rng.startContainer.childNodes.length) {
 			refNode.parentNode.appendChild(node);
 		} else {
 			var refNode = rng.startContainer.childNodes[rng.startOffset];
 			refNode.parentNode.insertBefore(node, refNode);
 		}
-	}	
+	}
 }
-
 
 if (!Array.prototype.map) {
 	Array.prototype.map = function(fun) {
 		var collect = [];
-		for (var ix = 0; ix < this.length; ix++) { collect[ix] = fun(this[ix]); }
+		for ( var ix = 0; ix < this.length; ix++) {
+			collect[ix] = fun(this[ix]);
+		}
 		return collect;
 	}
 }
